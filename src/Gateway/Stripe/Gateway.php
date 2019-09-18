6b3fe7ce47ae83cec4cd2d413969e48eb0d04801
Mickaël STAMM <contact@sta2m.com>
2019-09-18 17:55:49 +0100

    https://github.com/pi-engine/guide/issues/1739

--- src/Gateway/Stripe/Gateway.php
+++ src/Gateway/Stripe/Gateway.php
@@ -11,7 +11,7 @@
  * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
  */
 
-namespace Module\Order\Gateway\Paypal;
+namespace Module\Order\Gateway\Stripe;
 
 use Module\Order\Gateway\AbstractGateway;
 use Pi;
@@ -22,218 +22,12 @@ class Gateway extends AbstractGateway
     public function __construct()
     {
         parent::__construct();
-        $this->_type      = AbstractGateway::TYPE_REST;
+        $this->_type      = AbstractGateway::TYPE_STRIPE;
     }
 
     public function getType()
     {
         return $this->_type;
-    }
-
-
-    public function getApproval($order)
-    {
-        $url    = '/payments/payment';
-        $header = [
-            "Content-Type: application/json",
-            "Authorization: Bearer " . $token,
-        ];
-
-        // see here : https://developer.paypal.com/docs/integration/direct/payments/paypal-payments/#create-paypal-payment
-        $discount = null;
-
-
-        $items    = [];
-        $subtotal = 0;
-        $tax      = 0;
-        for ($i = 1; $i < $this->gatewayPayInformation['nb_product']; ++$i) {
-            $item                = [];
-            $item["name"]        = addcslashes($this->gatewayPayInformation['item_name_' . $i], '"');
-            $item["price"]       = $this->gatewayPayInformation['amount_' . $i];
-            $item["currency"]    = $this->gatewayPayInformation['currency_code'];
-            $item["quantity"]    = $this->gatewayPayInformation['quantity_' . $i];
-            $item["description"] = addcslashes($this->gatewayPayInformation['item_name_' . $i], '"');
-            $item["tax"]         = $this->gatewayPayInformation['tax_' . $i];
-            $items[]             = $item;
-
-            if ($this->gatewayPayInformation['discount_price_' . $i] > 0) {
-                $item             = [];
-                $item["name"]     = __('Discount');
-                $item["price"]    = -$this->gatewayPayInformation['discount_price_' . $i];
-                $item["currency"] = $this->gatewayPayInformation['currency_code'];
-                $item["quantity"] = 1;
-                $item["tax"]      = 0;
-                $items[]          = $item;
-            }
-
-            if ($this->gatewayPayInformation['unconsumed_' . $i] > 0) {
-
-                $item             = [];
-                $item["name"]     = __('Old package recovery');
-                $item["price"]    = -$this->gatewayPayInformation['unconsumed_' . $i];
-                $item["currency"] = $this->gatewayPayInformation['currency_code'];
-                $item["quantity"] = 1;
-                $item["tax"]      = 0;
-                $items[]          = $item;
-            }
-
-            $subtotal += $this->gatewayPayInformation['amount_' . $i] - $this->gatewayPayInformation['discount_price_' . $i]
-                - $this->gatewayPayInformation['unconsumed_' . $i];
-            $tax      += $this->gatewayPayInformation['tax_' . $i];
-        }
-        $data
-            = '{
-            "intent":"sale",
-            "redirect_urls":{
-                "return_url":"' . addcslashes($this->gatewayFinishUrl, '"') . '",
-                "cancel_url":"' . addcslashes($this->gatewayCancelUrl, '"') . '"
-            },
-            "payer":{
-                "payment_method":"paypal",
-                "payer_info":{
-                    "first_name":"' . addcslashes($this->gatewayPayInformation['first_name'], '"') . '",
-                    "last_name":"' . addcslashes($this->gatewayPayInformation['last_name'], '"') . '",
-                    "email":"' . addcslashes($this->gatewayPayInformation['email'], '"') . '"
-                }
-            },
-            "transactions":[
-                {
-                    "amount": {
-                        "total": "' . addcslashes(number_format($subtotal + $tax, 2, '.', ''), '"') . '",
-                        "currency": "' . addcslashes($this->gatewayPayInformation['currency_code'], '"') . '",
-                        "details": {
-                            "subtotal": "' . addcslashes(number_format($subtotal, 2, '.', ''), '"') . '",
-                            "tax": "' . addcslashes(number_format($tax, 2, '.', ''), '"') . '"
-                        }
-                    },
-                    "description": "",
-                    "item_list": {
-                        "items": ' . json_encode($items) . ',
-                        "shipping_address": {
-                            "recipient_name": "' . addcslashes(
-                $this->gatewayPayInformation['first_name'] . ' ' . $this->gatewayPayInformation['last_name'], '"'
-            ) . '",
-                            "line1": "' . addcslashes($this->gatewayPayInformation['address1'], '"') . '",
-                            "line2": "' . addcslashes($this->gatewayPayInformation['address2'], '"') . '",
-                            "city": "' . addcslashes($this->gatewayPayInformation['city'], '"') . '",
-                            "state": "' . addcslashes($this->gatewayPayInformation['state'], '"') . '",
-                            "phone": "' . addcslashes($this->gatewayPayInformation['night_phone_b'], '"') . '",
-                            "postal_code": "' . addcslashes($this->gatewayPayInformation['zip'], '"') . '",
-                            "country_code": "' . addcslashes($this->gatewayOption['location'], '"') . '"
-                        }
-                    }
-                }
-            ]
-        }';
-        $this->setLog($data, 'approval_start');
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        $this->setLog($result, 'approval_stop');
-
-        if (empty($result)) {
-            return null;
-        } else {
-            $result = json_decode($result);
-        }
-
-        $extra                      = json_decode($order['extra'], true);
-        $extra['paypal_payment_id'] = $result->id;
-        Pi::model('order', 'order')->update(
-            ['extra' => json_encode($extra)],
-            ['id' => $order['id']]
-        );
-
-        foreach ($result->links as $link) {
-            if ($link->rel == 'approval_url') {
-                return $link->href;
-            }
-        }
-
-        return null;
-
-    }
-
-
-    public function setAdapter()
-    {
-        $this->gatewayAdapter = 'Stripe';
-    }
-
-    public function setInformation()
-    {
-        $gateway                  = [];
-        $gateway['title']         = __('Stripe');
-        $gateway['path']          = 'Stripe';
-        $gateway['type']          = 'online';
-        $gateway['version']       = '1.0';
-        $gateway['description']   = '';
-        $gateway['author']        = 'Mickael STAMM <contact@sta2m.com>';
-        $gateway['credits']       = '@sta2m';
-        $gateway['releaseDate']   = 1567692940;
-        $this->gatewayInformation = $gateway;
-        return $gateway;
-    }
-
-    public function setSettingForm()
-    {
-        $form = [];
-        // form path
-        $form['path'] = [
-            'name'     => 'path',
-            'label'    => __('path'),
-            'type'     => 'hidden',
-            'required' => true,
-        ];
-
-        $form['onemail'] = [
-            'name'     => 'onemail',
-            'label'    => __('Send only 1 mail after payment (only for end user, not for admin)'),
-            'type'     => 'checkbox',
-            'required' => false,
-        ];
-
-        // business
-        $form['business'] = [
-            'name'     => 'business',
-            'label'    => __('Paypal email address'),
-            'type'     => 'text',
-            'required' => true,
-        ];
-
-        $form['invoice_name'] = [
-            'name'     => 'invoice_name',
-            'label'    => __('Gateway Name on the Invoice'),
-            'type'     => 'text',
-            'required' => false,
-        ];
-        // currency
-        $form['currency'] = [
-            'name'     => 'currency',
-            'label'    => __('Stripe currency'),
-            'type'     => 'text',
-            'required' => true,
-        ];
-
-        // test_mode
-        $form['error_mode']       = [
-            'name'     => 'error_mode',
-            'label'    => __('Force error for testing'),
-            'type'     => 'checkbox',
-            'required' => false,
-        ];
-        $this->gatewaySettingForm = $form;
-        return $this;
     }
 
     public function getAuthority()
@@ -247,7 +41,7 @@ class Gateway extends AbstractGateway
         // Set products to payment
         $i = 1;
         foreach ($products as $product) {
-            $this->gatewayPayInformation['item_name_' . $i]      = $product['details']['title'];
+            $this->gatewayPayInformation['item_name_' . $i]      = str_replace('<br>', ' : ',  $product['details']['title']);
             $this->gatewayPayInformation['item_number_' . $i]    = $product['number'];
             $this->gatewayPayInformation['quantity_' . $i]       = 1;
             $this->gatewayPayInformation['amount_' . $i]         = $product['product_price'];
@@ -301,13 +95,199 @@ class Gateway extends AbstractGateway
         }
     }
 
+    public function getSession($order)
+    {
+        \Stripe\Stripe::setApiKey($this->gatewayOption['password']);
+
+        $items    = [];
+        $subtotal = 0;
+        $tax      = 0;
+        for ($i = 1; $i < $this->gatewayPayInformation['nb_product']; ++$i) {
+            $item                = [];
+            $item["name"]        = addcslashes($this->gatewayPayInformation['item_name_' . $i], '"');
+            $item["amount"]       = ($this->gatewayPayInformation['amount_' . $i] + $this->gatewayPayInformation['tax_' . $i]) * 100;
+            $item["currency"]    = $this->gatewayPayInformation['currency_code'];
+            $item["quantity"]    = $this->gatewayPayInformation['quantity_' . $i];
+            $item["description"] = addcslashes($this->gatewayPayInformation['item_name_' . $i], '"');
+            $items[]             = $item;
+
+
+            $subtotal += $this->gatewayPayInformation['amount_' . $i] - $this->gatewayPayInformation['discount_price_' . $i]
+                - $this->gatewayPayInformation['unconsumed_' . $i];
+            $tax      += $this->gatewayPayInformation['tax_' . $i];
+        }
+
+
+/*
+            if ($this->gatewayPayInformation['discount_price_' . $i] > 0) {
+                $item             = [];
+                $item["name"]     = __('Discount');
+                $item["price"]    = -$this->gatewayPayInformation['discount_price_' . $i];
+                $item["currency"] = $this->gatewayPayInformation['currency_code'];
+                $item["quantity"] = 1;
+                $item["tax"]      = 0;
+                $items[]          = $item;
+            }
+
+            if ($this->gatewayPayInformation['unconsumed_' . $i] > 0) {
+
+                $item             = [];
+                $item["name"]     = __('Old package recovery');
+                $item["price"]    = -$this->gatewayPayInformation['unconsumed_' . $i];
+                $item["currency"] = $this->gatewayPayInformation['currency_code'];
+                $item["quantity"] = 1;
+                $item["tax"]      = 0;
+                $items[]          = $item;
+            }
+
+*/
+
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => $items,
+            'success_url' => $this->gatewayPayInformation['return'] . '?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => $this->gatewayPayInformation['cancel_return'],
+            'client_reference_id' => 'order-' . $order['id'] . '-uid-' . $order['uid'],
+            //'mode' => 'setup'
+        ]);
+
+        return $session;
+
+    }
+
+
+    public function setAdapter()
+    {
+        $this->gatewayAdapter = 'Stripe';
+    }
+
+    public function setInformation()
+    {
+        $gateway                  = [];
+        $gateway['title']         = __('Stripe');
+        $gateway['path']          = 'Stripe';
+        $gateway['type']          = 'online';
+        $gateway['version']       = '1.0';
+        $gateway['description']   = '';
+        $gateway['author']        = 'Mickael STAMM <contact@sta2m.com>';
+        $gateway['credits']       = '@sta2m';
+        $gateway['releaseDate']   = 1567692940;
+        $this->gatewayInformation = $gateway;
+        return $gateway;
+    }
+
+    public function setSettingForm()
+    {
+        $form = [];
+        // form path
+        $form['path'] = [
+            'name'     => 'path',
+            'label'    => __('path'),
+            'type'     => 'hidden',
+            'required' => true,
+        ];
+
+        $form['onemail'] = [
+            'name'     => 'onemail',
+            'label'    => __('Send only 1 mail after payment (only for end user, not for admin)'),
+            'type'     => 'checkbox',
+            'required' => false,
+        ];
+
+        // business
+        $form['business'] = [
+            'name'     => 'business',
+            'label'    => __('Stripe email address'),
+            'type'     => 'text',
+            'required' => true,
+        ];
+
+        $form['invoice_name'] = [
+            'name'     => 'invoice_name',
+            'label'    => __('Gateway Name on the Invoice'),
+            'type'     => 'text',
+            'required' => false,
+        ];
+        // currency
+        $form['currency'] = [
+            'name'     => 'currency',
+            'label'    => __('Stripe currency'),
+            'type'     => 'text',
+            'required' => true,
+        ];
+
+        // Username
+        $form['username'] = [
+            'name'     => 'username',
+            'label'    => __('Public key'),
+            'type'     => 'text',
+            'required' => false,
+        ];
+        // password
+        $form['password'] = [
+            'name'     => 'password',
+            'label'    => __('Secret key'),
+            'type'     => 'text',
+            'required' => false,
+        ];
+
+        // test_mode
+        $form['error_mode']       = [
+            'name'     => 'error_mode',
+            'label'    => __('Force error (for testing only)'),
+            'type'     => 'checkbox',
+            'required' => false,
+        ];
+        $this->gatewaySettingForm = $form;
+        return $this;
+    }
+
+    public function setPayForm()
+    {
+        return;
+    }
+
     public function setRedirectUrl()
     {
         $this->getAuthority();
+
     }
 
     public function verifyPayment($request, $processing)
     {
+
+        \Stripe\Stripe::setApiKey($this->gatewayOption['password']);
+
+        $session = \Stripe\Checkout\Session::Retrieve($request['stripe_session_id']);
+        $pi = $session['payment_intent'];
+        $payment = \Stripe\PaymentIntent::retrieve($pi);
+
+
+        $order     = Pi::api('order', 'order')->getOrder($processing['order']);
+        $extra     = json_decode($order['extra']);
+
+        if ($payment['status'] == 'succeeded') {
+            $result['status']  = 1;
+            $result['adapter'] = $this->gatewayAdapter;
+            $result['order']   = $order['id'];
+        } else {
+            $result['status']   = 0;
+            $result['state']    = $payment['status'] ;
+            $this->gatewayError = __('An error occured with Stripe. Please contact administrator');
+        }
+
+        return $result;
+
+
+        /*
+                $setup = \Stripe\SetupIntent::retrieve($session['setup_intent']);
+                $ayment = \Stripe\PaymentIntent::create([
+                    'amount' => 2000,
+                    'currency' => 'eur',
+                    'payment_method_types' => ['card'],
+                ]);*/
+        return false;
         // Need
     }
 
@@ -323,18 +303,13 @@ class Gateway extends AbstractGateway
         $this->gatewayError = '';
     }
 
-    function setPayForm()
-    {
-        return;
-    }
 
     public function getDescription()
     {
         return __(
-            'To install the Paypal Driver (Rest API) : <br>
-Declare your REST API App on https://developer.paypal.com/developer/applications/ : you will get the API Credentials (Client ID and Secret Key)<br>
-You can use the sandbox mode for your test and also check the error mode (no payment), to fine tune your code<br>
-You can get testing accounts from https://developer.paypal.com/developer/accounts/ : xxx-facilitator@xxx.com (seller) and xxx-buyer@xxx.com (buyer). You can use the pass of your normal account<br>
+            'To install the Stripe Driver : <br>
+Get yours credentials on stripe. <br>
+You can use the developper mode with developpers keys proveded by stripe for your test and also check the error mode (no payment), to fine tune your code<br>
         '
         );
     }
