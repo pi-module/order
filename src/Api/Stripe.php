e00f2273fdb8fcc2e773f4255912564a0b6ebfc5
Mickaël STAMM <contact@sta2m.com>
2019-09-25 17:30:08 +0100

    https://github.com/pi-engine/guide/issues/1739

---
+++ src/Api/Stripe.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Pi Engine (http://piengine.org)
+ *
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt New BSD License
+ */
+
+/**
+ * @author Mickaël STAMM <contact@sta2m.com>
+ */
+
+namespace Module\Order\Api;
+
+use Module\Order\Gateway\AbstractGateway;
+use Pi;
+use Pi\Application\Api\AbstractApi;
+
+
+class Stripe extends AbstractApi
+{
+    public function getOrderByTransfertIds($transfertIds)
+    {
+        if (empty($transfertIds) || count($transfertIds) == 0) {
+            return [];
+        }
+
+        foreach ($transfertIds as $transfert) {
+            $whereId[]  = 'extra LIKE "%\"transfer\":\"' . $transfert . '\"%"';
+        }
+        $where = implode(' OR ', $whereId);
+
+        $select = Pi::model('order', 'order')->select()->where($where);
+        $rowset = Pi::model('order', 'order')->selectWith($select);
+        $list = [];
+        foreach ($rowset as $row) {
+            $list[] = $row->toArray();
+        }
+        return $list;
+    }
+}	
